efa9508a4938db3b150c4796ec2c46fe5d7c2944
Matt Speed <matt.speed@focusrite.com>
2023-10-25 22:42:38 +0100

    Trying to brute-force using primes below half val, but it's taking too long to build the sieve.

--- 3/prime_factors.cpp
+++ 3/prime_factors.cpp
@@ -3,28 +3,36 @@ using std::endl; using std::cout;
 #include<vector>
 using std::vector;
 
+#include<algorithm>
+using std::reverse;
+
 class Prime
 {
 
 public:
-  [[nodiscard]] static vector<int> FindPrimeFactors(int value)
+  [[nodiscard]] static vector<uint64_t> FindPrimeFactors(uint64_t value)
   {
 
-    return vector<int>{};
-  }  
-
-  [[nodiscard]] static vector<int> Sieve(int value)
+    return vector<uint64_t>{};
+  }
+  
+  [[nodiscard]] static vector<uint64_t> Sieve(uint64_t value)
   {
-    vector<bool> n = {false};
-    vector<int> pv;
+
+    //cout << "Calling sieve for : " << value << endl;
     
-    for(int i = 2; i < value; i++)
+    vector<bool> n;
+    //for(int i = 0; i <= value; i++) n.push_back(false);
+    n.reserve(value+1);
+    vector<uint64_t> pv;
+    
+    for(uint64_t i = 2; i <= value; i++)
     {
       if(n[i]) continue;
 
       pv.push_back(i);
-      int test = i;
-      while(test < value)
+      uint64_t test = i;
+      while(test <= value)
       {
 	n[test] = true;
 	test += i;
@@ -33,17 +41,40 @@ public:
         
     return pv;
   }
+
+  [[nodiscard]] static uint64_t FindLargestPrimeFactor(uint64_t value)
+  {
+    vector<uint64_t> primes = Sieve(value >> 1);
+    //std::reverse(primes.begin(), primes.end());
+
+    cout << primes.size() << " primes in the sieve." << endl;
+
+    for(int i : primes)
+    {
+      if(!(value % i)) return i;
+    }
+            
+    return value;
+  }
   
 };
 
 
-int main()
+int main(int argc, char* argv[])
 {
-  for(int i : Prime::Sieve(100))
+  if(2!=argc){ cout << argv[0] << " <testint>" << endl; return 0; }
+  //int const testval = atoi(argv[1]);
+  uint64_t const testval = 600851475143;
+
+  vector<uint64_t> test = Prime::Sieve(testval);
+  /*
+  for(int i : test)
   {
     cout << i << ", ";
   }
-  cout << endl;
+  cout << endl;*/
+  
+  cout << "Largest prime factor: " << Prime::FindLargestPrimeFactor(testval) << endl;
 
 
 }

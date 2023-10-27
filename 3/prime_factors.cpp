179921ac2f0840ffc6d980eb5d35722b602c3827
Matt Speed <matt.speed@focusrite.com>
2023-10-27 17:31:28 +0100

    Testing incrementing primes according to a sieve.

--- 3/prime_factors.cpp
+++ 3/prime_factors.cpp
@@ -10,11 +10,6 @@ class Prime
 {
 
 public:
-  [[nodiscard]] static vector<uint64_t> FindPrimeFactors(uint64_t value)
-  {
-
-    return vector<uint64_t>{};
-  }
   
   [[nodiscard]] static vector<uint64_t> Sieve(uint64_t value)
   {
@@ -42,7 +37,8 @@ public:
     return pv;
   }
 
-  [[nodiscard]] static uint64_t FindLargestPrimeFactor(uint64_t value)
+    
+  [[nodiscard]] static uint64_t FindLargestPrimeFactor_old(uint64_t value)
   {
     vector<uint64_t> primes = Sieve(value >> 1);
     //std::reverse(primes.begin(), primes.end());
@@ -56,25 +52,62 @@ public:
             
     return value;
   }
+
+  [[nodiscard]] static vector<uint64_t> FindPrimeFactors(uint64_t value)
+  {
+    vector<bool> n;
+    n.reserve(value >> 1);
+
+    vector<uint64_t> p;
+    
+    // Loop, testing with numbers incrementing in a sieve
+    while(value > 1)
+    {
+      for(uint64_t i = 2; i <= value; i++)
+      {
+	if(n[i]) continue;
+	if(i > (value>>1)) i = value; 
+	
+	// Mod without loss
+	uint64_t const quotient = value/i;
+	uint64_t const remainder = value - (quotient*i);
+	
+	if(!remainder)
+	{
+	  value = quotient;
+	  p.push_back(i);
+	}
+	if(1 == value) return p;
+
+	uint64_t sieve_mul = i;
+	while(sieve_mul <= (value >> 1))
+	{
+	  n[sieve_mul] = true;
+	  sieve_mul += i;
+	}	
+      }
+    }
+    return vector<uint64_t>{value};
+  }
+
+  [[nodiscard]] static uint64_t FindLargestPrimeFactor(uint64_t value)
+  {
+    return *(FindPrimeFactors(value).end()-1);
+  }
   
 };
 
 
 int main(int argc, char* argv[])
 {
-  if(2!=argc){ cout << argv[0] << " <testint>" << endl; return 0; }
+  //if(2!=argc){ cout << argv[0] << " <testint>" << endl; return 0; }
   //int const testval = atoi(argv[1]);
   uint64_t const testval = 600851475143;
 
-  vector<uint64_t> test = Prime::Sieve(testval);
-  /*
-  for(int i : test)
-  {
-    cout << i << ", ";
-  }
-  cout << endl;*/
+  //for(int i : Prime::FindPrimeFactors(testval)) cout << i << ", ";
+  //cout << endl;
   
   cout << "Largest prime factor: " << Prime::FindLargestPrimeFactor(testval) << endl;
-
+  
 
 }

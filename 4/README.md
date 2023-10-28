418293aee68b4b7de0e08a17c71540eda4b3161e
Matt Speed <matt.speed@focusrite.com>
2023-10-28 23:05:50 +0100

    Setting up a function to test if an integer is palindromic.

---
+++ 4/README.md
@@ -0,0 +1,16 @@
+#Largest Palindrome Product
+
+The largest palindrome made from the product of two 2-digit integers is:
+9009 = 91 * 99
+
+Find the largest palindrome made from the product of two 3-digit integers.
+
+--
+
+Will be smaller than 1000*1000 = 1,000,000
+
+Ideas:
+- There are 1999 palindromic numbers below 10^6
+- Run a bunch of tests then check if the results are palindromic.
+
+
--- 4/palindrome.cpp
+++ 4/palindrome.cpp
@@ -1,1 +1,48 @@
 // Find the largest palindromic number made by the product of two three-digit numbers.
+
+#include<stack>
+using std::stack;
+
+#include<string>
+using std::string;
+
+#include<iostream>
+using std::cout; using std::endl;
+
+class Pal
+{
+public:
+
+  template<int N>
+  [[nodiscard]] static bool IsPalindrome(int val)
+  {
+    stack<char> vals;
+    string const num = std::to_string(val);
+
+    int i;
+    for(i = 0; i < (N>>1); i++)
+    {
+      vals.push(num[i]);
+    }
+    if(N&1) i++;
+    for(    ; i < N; i++)
+    {
+      if(vals.top() != num[i]) return false;
+      vals.pop();
+    }
+
+    return true;
+    
+  };
+
+
+};
+
+int main()
+{
+  int const test = 1234325;
+  cout << "test: " << test << " is ";
+  if(!Pal::IsPalindrome<7>(test)) cout << "not ";
+  cout << "palindromic." << endl;
+
+}

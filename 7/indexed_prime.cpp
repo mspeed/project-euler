4a7fb19ae0b036f9b49c1cad0b3f67bd0b3f0a0f
Matt Speed <matt.speed@focusrite.com>
2023-11-01 23:08:41 +0000

    Recursive referential prime sieve failing somehow.

--- 7/indexed_prime.cpp
+++ 7/indexed_prime.cpp
@@ -5,28 +5,56 @@ using std::endl; using std::cout;
 #include<vector>
 using std::vector;
 
+#include<array>
+using std::array;
 
+
+template<int HIGHEST_TEST_VALUE>
 class cSieve
 {
 public:
 
   struct sPrimeInfo
   {
-    //vector<bitset<64>}}
-    vector<bool> Sieve;
+    sPrimeInfo(){ for(bool b : Sieve){ b = false; } }
+    array<bool, HIGHEST_TEST_VALUE> Sieve;
     vector<uint64_t> Primes;
   };
 
   [[nodiscard]] static sPrimeInfo& GetNthPrime(int N, sPrimeInfo& P)
   {
+    if(N >= HIGHEST_TEST_VALUE) return P;
     // N'th prime is not divisible by the N-1 primes that precede it.
-    
+    if(!N){ P.Primes.push_back(1); P.Sieve[0] = true; return P; }
     // Do we really want 10k stack frames? Probably not.
 
-    if(N) P = GetNthPrime(N-1, P);      
-
     // Find the Nth prime using the sieve in P.
-    P.Primes.push_back(N);
+    P = GetNthPrime(N-1, P);      
+    
+    uint64_t const LastPrime = P.Primes[P.Primes.size()-1];
+
+    cout << "LastPrime: " << LastPrime << endl;
+    
+    uint64_t PrimeTest = LastPrime + 1;
+    while(P.Sieve[PrimeTest])
+    {
+      cout << "P.Sieve[" << PrimeTest << " = " << P.Sieve[PrimeTest] << endl;
+      PrimeTest++;      
+    }
+
+    cout << "PrimeTest leads to: " << PrimeTest << endl;
+
+    P.Primes.push_back(PrimeTest);
+    P.Sieve[PrimeTest] = true;
+    
+    uint64_t ClearIndex = PrimeTest;
+    
+    while(ClearIndex < HIGHEST_TEST_VALUE)
+    {
+      cout << "Setting sieve[" << ClearIndex << "] to true." << endl;
+      P.Sieve[ClearIndex] = true;
+      ClearIndex += PrimeTest;
+    }
     
     return P;
   };
@@ -40,12 +68,17 @@ public:
 int main()
 {
 
-  cSieve::sPrimeInfo mPI;
+  
+  cSieve<20>::sPrimeInfo mPI;
+  
 
-  mPI = cSieve::GetNthPrime(5, mPI);
+  mPI = cSieve<20>::GetNthPrime(5, mPI);
 
 
-  for(uint64_t i : mPI.Primes) cout << i << ", " << endl; 
+  for(uint64_t i : mPI.Primes) cout << i << ", " << endl;
+
+  cout << "Found: " << mPI.Primes.size() << " primes." << endl;
+  //cout << "Last prime: " << *(mPI.Primes.end()--) << endl;
   
 
 
